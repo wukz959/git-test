df64d46ae9a0dd8d9ab5163c2a65502b1fc94870
wukz <666@qq.com>
2022-11-08 21:12:35 +0800

    Revert "Second Commit"
    This reverts commit ce0c737e

--- src/main/java/com/myworld/git/GitTest.java
+++ src/main/java/com/myworld/git/GitTest.java
@@ -4,6 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
     }
 }
