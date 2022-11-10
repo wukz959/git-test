2ede75dedd249fbabd21df79a5767975fd3ab911
wukz <666@qq.com>
2022-11-10 20:21:53 +0800

    master commit1

--- src/main/java/com/myworld/git/GitTest.java
+++ src/main/java/com/myworld/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("I'm master");
         System.out.println("I'm hot-fix");
+        System.out.println("Hello!!!");
     }
 }
